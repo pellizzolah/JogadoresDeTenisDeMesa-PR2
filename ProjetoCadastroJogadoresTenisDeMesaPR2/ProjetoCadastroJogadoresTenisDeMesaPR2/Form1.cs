3f554b5c8fb8d746576f6164431ebbac837d496b
pellizzolah <pellizzolaleandro@gmail.com>
2025-11-04 10:45:31 -0300

    Add files via upload

---
+++ ProjetoCadastroJogadoresTenisDeMesaPR2/ProjetoCadastroJogadoresTenisDeMesaPR2/Form1.cs
@@ -0,0 +1,39 @@
+namespace ProjetoCadastroJogadoresTenisDeMesaPR2
+{
+    public partial class Form1 : Form
+    {
+
+        public string nomeusuario;
+        public string senhausuario;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void botaologin_Click(object sender, EventArgs e)
+        {
+
+
+            if (String.IsNullOrEmpty(usuariobox.Text) || String.IsNullOrEmpty(senhabox.Text)) //Verifica se algum dos campos não estão vazios
+            {
+                MessageBox.Show("ERRO! Um dos campos ou ambos não estao preenchidos! Note que ambos tem que estar com informações para efetuar o login!");
+            }
+            else
+            {
+                nomeusuario = usuariobox.Text;
+                senhausuario = senhabox.Text;
+            }
+
+
+
+
+
+
+
+        }
+
+      
+    }
+}
